aac5df7f5c2e64f58d08031b047dbf589f9df183
Xinjie Chen <1377232072@qq.com>
2025-12-22 14:26:47 +0800

    refactor: verify returns tuple of tensors now (#30)
    Make this method more simple and focused to 'verification' only. Leave
    the responsiblity of actual output construction for the caller.

--- tests/verify/test_verifier.py
+++ tests/verify/test_verifier.py
@@ -5,11 +5,12 @@ from naive_speculate.draft import Drafter
 from naive_speculate.verify import Verifier
 
 DRAFT_MODEL_NAME = "Qwen/Qwen3-0.6B"
+DRAFT_TOKENS_NUM = 20
 CONFIG_DICT = {
     "max_new_tokens": 32768,
     "decode_method": "greedy",
     "drafter_model_name": DRAFT_MODEL_NAME,
-    "draft_tokens_num": 20,
+    "draft_tokens_num": DRAFT_TOKENS_NUM + 1,
     "verifier_model_name": DRAFT_MODEL_NAME,
 }
 PROMPT_LENGTH = 16
@@ -25,11 +26,17 @@ def test_verifier(verifier: Verifier, drafter: Drafter):
     """Verify that the verifier works as expected when drafter model is the same as verify model."""
     input_ids = torch.randint(low=0, high=VOCAB_SIZE, size=(1, PROMPT_LENGTH))
 
+    drafter._reset()
     drafter.kv_cache.crop(0)
+    verifier._reset()
     verifier.kv_cache.crop(0)
     drafter_output_ids, drafter_output_logits = drafter.draft(input_ids)
-    verifier_output_ids = verifier.verify(drafter_output_ids, drafter_output_logits)
+    rejected_idx, resampled_token = verifier.verify(
+        drafter_output_ids[:, :-1],
+        drafter_output_logits[:, :-1],
+    )
 
-    batch_size, seq_len = drafter_output_ids.shape
-    assert verifier_output_ids.shape == (batch_size, seq_len + 1)
-    assert torch.all(verifier_output_ids[:, :-1] == drafter_output_ids)
+    assert rejected_idx.shape == torch.Size()
+    assert resampled_token.shape == torch.Size()
+    assert rejected_idx.item() == DRAFT_TOKENS_NUM
+    assert resampled_token.item() == drafter_output_ids[0, -1].item()
