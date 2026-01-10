992b4b3fb47a2a23e7a0df4be531637a71a8a6f1
Xinjie Chen <1377232072@qq.com>
2026-01-10 11:47:30 +0800

    test: refactor kvcache tests (#68)
    Delay the num_tokens_crop to test function to couple the logic.
    Add some docstring and utility.
    Simplify file name.

--- src/naive_speculate/testing/infer/kvcache/__init__.py
+++
@@ -1,4 +0,0 @@
-from .constants import KVSTATES, NUM_TOKENS_CROP
-from .contract_tests import KVCacheContractTests
-
-__all__ = ["KVSTATES", "NUM_TOKENS_CROP", "KVCacheContractTests"]
--- tests/unit/infer/kvcache/test_dynamic.py
+++ tests/unit/infer/kvcache/test_dynamic.py
@@ -3,7 +3,9 @@ from typing import TYPE_CHECKING
 import pytest
 
 from naive_speculate.infer.kvcache.dynamic_cache import DynamicCache
-from naive_speculate.testing.infer.kvcache import NUM_TOKENS_CROP, KVCacheContractTests
+from naive_speculate.testing.infer.kvcache.constants import CROP_RATIOS
+from naive_speculate.testing.infer.kvcache.contract import KVCacheContractTests
+from naive_speculate.testing.infer.kvcache.utils import get_num_tokens_crop
 
 if TYPE_CHECKING:
     from naive_speculate.infer import KVState
@@ -19,9 +21,13 @@ class TestDynamicCacheContract(KVCacheContractTests):
     ) -> None:
         super().update_test(dynamic_cache, kv_states)
 
-    @pytest.mark.parametrize("num_tokens_crop", NUM_TOKENS_CROP)
+    @pytest.mark.parametrize("crop_ratio", CROP_RATIOS)
     def test_dynamic_cache_crop(
-        self, dynamic_cache: DynamicCache, kv_states: list[KVState], num_tokens_crop: int
+        self, dynamic_cache: DynamicCache, kv_states: list[KVState], crop_ratio: float
     ) -> None:
         dynamic_cache.update(kv_states)
+
+        num_tokens = dynamic_cache.get_num_tokens()
+        num_tokens_crop = get_num_tokens_crop(num_tokens, crop_ratio)
+
         super().crop_test(dynamic_cache, num_tokens_crop)
